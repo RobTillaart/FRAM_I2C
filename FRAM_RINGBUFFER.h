5650ce50608c9976ff05d138a4998b5f24ccde40
rob tillaart <rob.tillaart@gmail.com>
2022-10-04 22:24:54 +0200

    fix peek(obj, size)

--- FRAM_RINGBUFFER.h
+++ FRAM_RINGBUFFER.h
@@ -164,6 +164,7 @@ public:
     uint32_t tmp = _tail;    //  remember _tail 'pointer'
     int n = read(data, objectSize);
     _tail = tmp;             //  restore _tail 'pointer'
+    _count += n;
     return n;
   }
 
